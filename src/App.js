3684cf1d94d0b27d99897d28ac6f4850231acba7
andrewhoward96 <99508807+andrewhoward96@users.noreply.github.com>
2024-04-03 12:53:41 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          making changes on git and vercel
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+ const [data,setData] = useState();
+  const [name,setName] = useState();
+  const [weight,setWeight] = useState();
+  const [number,setNumber] = useState(1);
+  const [showData, setShowData] = useState(false);
+
+const DndApi = 'https://www.dnd5eapi.co/api/'
+
+useEffect(() => {
+  axios.get(`${DndApi}${number}`).then((res) => {
+    console.log(res.data)
+  setData(res.data);
+  setName(res.data.name);
+  setWeight(res.data.weight);
+
+  }).catch((err)=>{
+    window.alert(err);
+  });
+}, [number]);
+
+
+
+    return (
+      <div className="App">
+        <h1>dnd</h1>
+        <input type="number" onChange={(e) => {e.target.value && setNumber(e.target.value)}} />
+        <h2>name of dnd :{name}</h2>
+        <button onClick={() => setShowData(!showData)}>Show {showData ? 'Less' : 'More'} Info</button>
+        {showData && (
+          <>
+            <h3>weight of dnd : {weight}</h3>
+            <img src={data ? data.front_default : "<p>loading</p>"} alt="dnd"/>
+            <p>my abilities are:</p>
+            {data? data.abilities.map((value,key)=>{
+              return(
+                <div key={key}>
+                  {value.ability.name}
+                </div>
+              )
+            }):""}
+          </>
+        )}
     </div>
   );
 }
